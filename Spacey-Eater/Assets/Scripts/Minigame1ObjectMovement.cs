ab3d05bb1dbc7935f0d5356712cdb2d5e0d3226b
Shunta Hashimoto <schashim@usc.edu>
2019-03-06 21:12:52 -0800

    updated minigame 1

---
+++ Spacey-Eater/Assets/Scripts/Minigame1ObjectMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minigame1ObjectMovement : MonoBehaviour
+{
+    public float leftAndRightSpeed = 1f;
+    public float topAndBottomSpeed = 1f;
+    public float leftAndRightEdge = 5f;
+    public float topAndBottomEdge = 2f;
+    public float chanceToChangeDirection = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Basic Movement
+        Vector2 pos = transform.position;
+        pos.x += leftAndRightSpeed * Time.deltaTime;
+        pos.y += topAndBottomSpeed * Time.deltaTime;
+        transform.position = pos;
+
+        //Changing direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            leftAndRightSpeed = Mathf.Abs(leftAndRightSpeed); 
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            leftAndRightSpeed = -Mathf.Abs(leftAndRightSpeed); 
+        }
+        //Changing direction
+        if (pos.x < -topAndBottomEdge)
+        {
+            topAndBottomSpeed = Mathf.Abs(topAndBottomSpeed);
+        }
+        else if (pos.x > topAndBottomSpeed)
+        {
+            topAndBottomSpeed = -Mathf.Abs(topAndBottomSpeed);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //Changing Direction Randomly is now time-based
+        if (Random.value < chanceToChangeDirection)
+        {
+            leftAndRightSpeed *= -1;
+        }
+
+        //Changing Direction Randomly is now time-based
+        if (Random.value < chanceToChangeDirection)
+        {
+            topAndBottomSpeed *= -1; //Change direction
+        }
+    }
+}
---
+++ Spacey-Eater/Assets/Scripts/TimerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Diagnostics;
+
+public class TimerController : MonoBehaviour
+{
+
+    private static float gameTime;
+    private Text timeGT;
+    public float maxGameTime = 10f;
+    private void Awake()
+    {
+        Text gt = this.GetComponent<Text>();
+        gameTime = maxGameTime;
+        gt.text = "" + (int)gameTime;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameTime -= Time.deltaTime;
+        Text gt = this.GetComponent<Text>();
+        gt.text = "" + (int)gameTime;
+    }
+}

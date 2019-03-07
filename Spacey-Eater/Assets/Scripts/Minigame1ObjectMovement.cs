7e10846e228e44f8aebad5b70a78ba2f2bc40c9e
Shunta Hashimoto <schashim@usc.edu>
2019-03-06 18:42:23 -0800

    Minigame 1 implemented onto shunta branch

---
+++ Spacey-Eater/Assets/Scripts/Minigame1ObjectMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minigame1ObjectMovement : MonoBehaviour
+{
+    public float leftAndRightSpeed = 1f;
+    public float topAndBottomSpeed = 1f;
+    public float leftAndRightEdge = 5f;
+    public float topAndBottomEdge = 2f;
+    public float chanceToChangeDirection = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Basic Movement
+        Vector3 pos = transform.position;
+    }
+}

ed57ee4882ebda5df931c0c45733e2a8b6c4c983
Shunta Hashimoto <schashim@usc.edu>
2019-03-19 15:15:23 -0700

    updated minigame 1 to integrate into main game

--- Spacey-Eater/Assets/Scripts/TouchInput.cs
+++ Spacey-Eater/Assets/Scripts/TouchInput.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class TouchInput : MonoBehaviour
 {
-    public float changePerClick = 1f;
+    public float changePerClick = 1.5f;
+    public bool didWin = false;
     // Start is called before the first frame update
     void Start()
     {
         bool supportsMultiTouch = Input.multiTouchEnabled;
+        didWin = false;
         if (supportsMultiTouch)
         {
             Debug.Log("MultiTouchSupport enabled");
@@ -37,31 +39,49 @@ public class TouchInput : MonoBehaviour
                     RaycastHit hit;
                     if (Physics.Raycast(screenRay, out hit))
                     {
-                        Debug.Log("User tapped on game object " + hit.collider.gameObject);
-                        Vector3 temp = hit.collider.gameObject.transform.localScale;
-                        hit.collider.gameObject.transform.localScale -= new Vector3(changePerClick, changePerClick, 0f);
-                        CircleCollider2D circleCollider = hit.collider.gameObject.GetComponent<CircleCollider2D>();
-                        circleCollider.transform.localScale -= new Vector3 (changePerClick, changePerClick, 0f);
+                        if (hit.collider.gameObject.tag == "Enemy")
+                        {
+                            Debug.Log("User tapped on game object " + hit.collider.gameObject);
+                            Vector3 temp = hit.collider.gameObject.transform.localScale;
+                            if (hit.collider.gameObject.transform.localScale.x < changePerClick || hit.collider.gameObject.transform.localScale.y < changePerClick)
+                            {
+                                //if user destroys object before time runs out didWin is true
+                                didWin = true;
+                                Debug.Log("didWin is True");
+                            }
+                            else
+                            {
+                                hit.collider.gameObject.transform.localScale -= new Vector3(changePerClick, changePerClick, 0f);
+                                hit.collider.gameObject.GetComponent<CircleCollider2D>().transform.localScale -= new Vector3(changePerClick, changePerClick, 0f);
+                                //Debug.Log(hit.collider.GetComponent<CircleCollider2D>().transform.localScale);
+                            }
+                        }
                     }
                 }
             }
         }
         else if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Mouse Input Registered");
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
 
             RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
             var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (hit.collider != null)
+            if (hit.collider.gameObject.tag == "Enemy")
             {
                 Debug.Log("User tapped on game object " + hit.collider.gameObject);
                 Vector3 temp = hit.collider.gameObject.transform.localScale;
-                hit.collider.gameObject.transform.localScale -= new Vector3(changePerClick, changePerClick, 0f);
-                hit.collider.gameObject.GetComponent<CircleCollider2D>().transform.localScale -= new Vector3(changePerClick, changePerClick, 0f);
-                Debug.Log(hit.collider.GetComponent<CircleCollider2D>().transform.localScale);
-
+                if (hit.collider.gameObject.transform.localScale.x < changePerClick || hit.collider.gameObject.transform.localScale.y < changePerClick)
+                {
+                    didWin = true;
+                    Debug.Log("didWin is True");
+                }
+                else
+                {
+                    hit.collider.gameObject.transform.localScale -= new Vector3(changePerClick, changePerClick, 0f);
+                    hit.collider.gameObject.GetComponent<CircleCollider2D>().transform.localScale -= new Vector3(changePerClick, changePerClick, 0f);
+                    //Debug.Log(hit.collider.GetComponent<CircleCollider2D>().transform.localScale);
+                }
             }
         }
     }

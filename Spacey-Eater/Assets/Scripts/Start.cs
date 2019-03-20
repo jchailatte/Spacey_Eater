a5720937fe5fd0ed71196ad79b6e898ed393a5ae
Jonathan Chai <chaijw@usc.edu>
2019-03-20 16:38:46 -0700

    win/lose/launch

---
+++ Spacey-Eater/Assets/Scripts/Start.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Launch()
+    {
+        Application.LoadLevel("Game");
+    }
+
+    public void ToLaunch()
+    {
+        Application.LoadLevel("Launch");
+    }
+}

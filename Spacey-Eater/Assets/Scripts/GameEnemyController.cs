a02f6c7ed7104e8d3a473dcf15e1759afcf38d3a
Shunta Hashimoto <schashim@usc.edu>
2019-03-19 17:44:34 -0700

    Added Scorekeeper to Main Game

---
+++ Spacey-Eater/Assets/Scripts/GameEnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnemyController : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+
+    public float speed = 2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Spacey-Eater/Assets/Scripts/GameScoreController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Diagnostics;
+using UnityEngine.SceneManagement;
+
+public class GameScoreController : MonoBehaviour
+{
+
+    private static float score;
+    private GameObject mainPlayer;
+
+    //Use awake to find score
+    private void Awake()
+    {
+        mainPlayer = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateScore();
+    }
+
+    //Update score
+    public void UpdateScore()
+    {
+        Text gt = this.GetComponent<Text>();
+        score = mainPlayer.transform.localScale.x * 10000f + 1;
+        gt.text = "Score: " + (int)score;
+    }
+}

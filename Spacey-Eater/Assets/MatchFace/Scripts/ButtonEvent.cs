826dce4a9ae029d03a997260aa226df48b2ef9ea
Jonathan Chai <chaijw@usc.edu>
2019-03-27 21:51:34 -0700

    match face minigame done

---
+++ Spacey-Eater/Assets/MatchFace/Scripts/ButtonEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonEvent : MonoBehaviour
+{
+
+    public Text buttonText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pressed()
+    {
+        Debug.Log(buttonText.text);
+        transform.parent.gameObject.GetComponent<OverallContMF>().checkAnswer(buttonText.text);
+    }
+}

dbe5b35f5fefbdcbe52184d56ac84a12e9b8e242
Anthony Klachin <klachin@usc.edu>
2019-04-24 17:13:44 -0700

    some fixes

--- Spacey-Eater/Assets/Scripts/requestUserAccess.cs
+++ Spacey-Eater/Assets/Scripts/requestUserAccess.cs
@@ -3,30 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.iOS;
 
-public class requestUserAccess : MonoBehaviour
-{
-    // Start is called before the first frame update
-    IEnumerator Start()
-    { 
-        findMicrophones();
+//public class requestUserAccess : MonoBehaviour
+//{
+//    // Start is called before the first frame update
+//    IEnumerator Start()
+//    { 
+//        findMicrophones();
 
-        yield return Application.RequestUserAuthorization(UserAuthorization.Microphone);
-        if (Application.HasUserAuthorization(UserAuthorization.Microphone))
-        {
-            Debug.Log("Microphone found");
-        }
-        else
-        {
-            Debug.Log("Microphone not found");
-        }
-    }
+//        yield return Application.RequestUserAuthorization(UserAuthorization.Microphone);
+//        if (Application.HasUserAuthorization(UserAuthorization.Microphone))
+//        {
+//            Debug.Log("Microphone found");
+//        }
+//        else
+//        {
+//            Debug.Log("Microphone not found");
+//        }
+//    }
 
-    void findMicrophones()
-    {
-        foreach (var device in Microphone.devices)
-        {
-            Debug.Log("Name: " + device);
-        }
-    }
+//    void findMicrophones()
+//    {
+//        foreach (var device in Microphone.devices)
+//        {
+//            Debug.Log("Name: " + device);
+//        }
+//    }
 
-}
+//}
